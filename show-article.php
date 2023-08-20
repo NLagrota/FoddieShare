f3c5c9f11e4b6a8b855ad1a37552ae57d24d7c73
Aleska <aleskavargas@gmail.com>
2023-08-20 02:23:45 -0400

    Estrelas

--- show-article.php
+++ show-article.php
@@ -41,6 +41,49 @@ if (!$id) {
   }
 }
 
+// Vérifier si l'utilisateur est connecté (c'est une étape simplifiée) -- dernier mod
+$isUserLoggedIn = true; // Par exemple, vérifiez si l'utilisateur est connecté.
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $isUserLoggedIn) {
+    // L'utilisateur a soumis un avis
+    $newReview = [
+        'utilisateur' => 'Nom de l\'utilisateur', // Remplacez par le nom de l'utilisateur connecté.
+        'note' => $_POST['note'],
+        'commentaire' => $_POST['commentaire'],
+    ];
+
+    // Ajoutez l'avis à l'article
+    $article['avis'][] = $newReview;
+
+    // Mettez à jour le fichier JSON avec le nouvel avis
+    file_put_contents($filename, json_encode($articles, JSON_PRETTY_PRINT));
+
+    // Redirigez pour éviter la soumission en double
+    header('Location: show-article.php?id=' . $id);
+}
+
+$note = 4; // Note donnée (peut être un nombre de 1 à 5)
+
+// Vérifiez si la note est dans la plage autorisée (1 à 5)
+if ($note < 1) {
+    $note = 1;
+} elseif ($note > 5) {
+    $note = 5;
+}
+
+// Fonction pour afficher les étoiles en fonction de la note
+function afficherEtoiles($note) {
+    $etoiles = '';
+    for ($i = 1; $i <= 5; $i++) {
+        if ($i <= $note) {
+            $etoiles .= '★'; // Étoile remplie
+        } else {
+            $etoiles .= '☆'; // Étoile vide
+        }
+    }
+    return $etoiles;
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -49,20 +92,77 @@ if (!$id) {
   <?php require_once 'includes/head.php' ?>
   <link rel="stylesheet" href="public/css/show-article.css">
   <title>Article</title>
+
+  <style>
+/* Style pour les étoiles de notation */
+.rating-stars {
+    font-size: 24px; /* Taille des étoiles */
+    color: #FFD700; /* Couleur des étoiles remplies */
+}
+
+.rating-stars::before {
+    content: "\2605"; /* Code unicode pour une étoile remplie */
+    margin-right: 4px; /* Espacement entre les étoiles */
+}
+
+.rating-stars::after {
+    content: "\2606"; /* Code unicode pour une étoile vide */
+}
+    </style>
+
+
+
 </head>
 
 <body>
-  <div class="container">
-    <?php require_once 'includes/header.php' ?>
-    <div class="content">
-      <div class="article-container">
-        <a class="article-back" href="./index.php">Retour à la liste des articles</a>
-        <div class="article-cover-img" style="background-image:url(<?=$article['image']?>)"></div>
-        <h1 class="article-title"><?=$article['titre']?></h1>
-        <div class="separator"></div>
-        <p class="article-content"><?=$article['contenu']?></p>
-      </div>
-    </div>
+<div class="container">
+        <?php require_once 'includes/header.php' ?>
+        <div class="content">
+            <div class="article-container">
+                <a class="article-back" href="./index.php">Retour à la liste des articles</a>
+                <div class="article-cover-img" style="background-image:url(<?=$article['image']?>)"></div>
+                <h1 class="article-title"><?=$article['titre']?></h1>
+                <div class="separator"></div>
+                <p class="article-content"><?=$article['contenu']?></p>
+
+                <!-- Afficher des étoiles de notation -->
+                <div class="rating-stars">
+                    <?php echo afficherEtoiles($note); ?>
+                </div>
+            </div>
+        </div>
+
+    <!-- Formulaire pour soumettre un avis -->
+
+    <?php if ($isUserLoggedIn) : ?>
+
+        <h2>Laissez un avis</h2>
+        <form method="post">
+            <label for="note">Note :</label>
+            <input type="number" name="note" min="1" max="5" required>
+
+            <label for="commentaire">Commentaire :</label>
+            <textarea name="commentaire" required></textarea>
+
+            <button type="submit">Soumettre l'avis</button>
+        </form>
+        <?php else : ?>
+        <p>Connectez-vous pour laisser un avis.</p>
+    <?php endif; ?>
+
+    <!-- Affichage des avis existants -->
+    <?php if (isset($article['avis'])) : ?>
+        <h2>Avis des utilisateurs</h2>
+        <ul>
+            <?php foreach ($article['avis'] as $review) : ?>
+                <li>
+                    <strong><?=$review['utilisateur']?></strong> 
+                    (Note : <?=$review['note']?>)
+                    <p><?=$review['commentaire']?></p>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
 
     <!-- Ajouter bouton pour modifier article --> 
       <div class="action">
