998ab7399a262dc8b478acf09c70cf409520750a
«Natalia <natalialagrota.ca@gmail.com>
2023-08-21 12:43:31 -0400

    ajustes do titulo

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-$filename = __DIR__. '/public/data/articles.json';
-$articles = []; 
+$filename = __DIR__ . '/public/data/articles.json';
+$articles = [];
 $categorie = [];
 $GET = filter_input_array(INPUT_GET, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $selectedCat = $_GET['cat'] ?? '';
 $keyword = $_GET['search'] ?? ''; // Récupérer le mot-clé de recherche
 
-function count_categories ($accumulateur, $valeur_courante)
+function count_categories($accumulateur, $valeur_courante)
 {
     if (isset($accumulateur[$valeur_courante])) {
         $accumulateur[$valeur_courante]++;
@@ -15,20 +15,20 @@ function count_categories ($accumulateur, $valeur_courante)
     }
     return $accumulateur;
 }
-    
+
 if (file_exists($filename)) {
-    $articles = json_decode(file_get_contents($filename), true) ?? []; 
+    $articles = json_decode(file_get_contents($filename), true) ?? [];
     // Reduire les cles du tableau en extrayant uniquement la categorie 
-    $cattmp = array_map(fn($a) => $a['categorie'], $articles); 
+    $cattmp = array_map(fn ($a) => $a['categorie'], $articles);
     //print_r($cattmp);=> Array ( [0] => chalets [1] => chalets [2] => hotels [3] => hotels 
     // [4] => camping [5] => camping)
-    
+
     //array_reduce est utilisé avec la fonction de rappel count_categories 
     // pour itérer sur le tableau $cattmp et accumuler les résultats dans le tableau $categories. 
-    $categories = array_reduce($cattmp, 'count_categories', []); 
+    $categories = array_reduce($cattmp, 'count_categories', []);
     //print_r($categories); => Array ( [chalets] => 2 [hotels] => 2 [camping] => 2 )
     //Pour chaque catégorie, énumérer tous les articles qui y sont associé :
-    
+
     $articlesParCategorie = array_reduce($articles, 'classifier_articles', []);
     // file_put_contents($test_filename, json_encode($articlesParCategorie));
     print_r(json_encode($articlesParCategorie));
@@ -37,9 +37,9 @@ if (file_exists($filename)) {
 // Filtrer les articles en fonction du mot-clé de recherche****************************
 if (!empty($keyword)) {
     $filteredArticles = array_filter($articles, function ($article) use ($keyword) {
-        return stripos($article['titre'], $keyword) !== false || 
-               stripos($article['description'], $keyword) !== false ||
-               stripos($article['categorie'], $keyword) !== false;
+        return stripos($article['titre'], $keyword) !== false ||
+            stripos($article['description'], $keyword) !== false ||
+            stripos($article['categorie'], $keyword) !== false;
     });
     // Utiliser $filteredArticles pour afficher les résultats
 }
@@ -53,13 +53,9 @@ function classifier_articles($acc, $article)
         $acc[$article['categorie']] = [...$acc[$article['categorie']], $article];
     } else {
         $acc[$article['categorie']] = [$article];
-    } 
+    }
     return $acc;
 }
-
-
-
-
 ?>
 
 
@@ -77,114 +73,111 @@ function classifier_articles($acc, $article)
 <body>
     <div class="container">
         <?php require_once 'includes/header.php' ?>
-    <div class="content">
-        <!-- Insérer le code ici pour afficher les résultats de la recherche****************************** -->
-    <?php if (!empty($filteredArticles)): ?>
-    <h2>Résultats de la recherche pour "<?php echo $keyword; ?>"</h2>
-    <div class="articles-container">
-        <?php foreach ($filteredArticles as $a): ?>
-            <a href="show-article.php?id=<?=$a['id']?>" class="article block">
-            <!--Image--->
-                    <div class="overflow">
-                        <div class="img-container" style="background-image:url(<?=$a['image']?>)"></div>
-                    </div>
-                    <!-- Titre avec mise en évidence du mot clé -->
-                    <h3>
-                        <?=highlightKeyword($a['titre'], $keyword)?>
-                    </h3>
-                    </a>
-                        <?php endforeach; ?>
-                    </div>
+        <div class="content">
+            <!-- Affichage des résultats de la recherche-->
+            <?php if (!empty($filteredArticles)) : ?>
+                <div class="fieldset">
+                    <fieldset>
+                        <h2>Résultats de la recherche pour "<?php echo $keyword; ?>"</h2>
+                        <div class="articles-container">
+                            <?php foreach ($filteredArticles as $a) : ?>
+                                <a href="show-article.php?id=<?= $a['id'] ?>" class="article block">
+                                    <!--Image--->
+                                    <div class="overflow">
+                                        <div class="img-container" style="background-image:url(<?= $a['image'] ?>)"></div>
+                                    </div>
+                                    <!-- Titre avec mise en évidence du mot clé -->
+                                    <h3>
+                                        <?= highlightKeyword($a['titre'], $keyword) ?>
+                                    </h3>
+                                </a>
+                            <?php endforeach; ?>
+                        </div>
+                    </fieldset>
+                </div>
 
-                    
-                <?php else: ?>
-                    <p>Aucun résultat trouvé pour "<?php echo $keyword; ?>"</p>
+
+
+            <?php else : ?>
+                <div class="noresult">
+                    <fieldset>
+                        <p>Aucun résultat trouvé pour "<?php echo $keyword; ?>"</p>
+                    </fieldset>
                 <?php endif; ?>
-
-
-     <div class="newsfeed-container"> 
-            <div class="categorie-container">
-                <ul>
-                    <!-- Tous les articles -->
-                    <li class=<?=$selectedCat ? '' : 'cat-active'?>>
-                        <a href="/"> Tous les repas <span class="small">(<?=count($articles)?>)</span></a>
-                    </li>
-                    <!-- Libelles de toutes les categories -->
-                    <?php foreach ($categories as $catName => $catNum): ?>
-                    <li class=<?=$selectedCat === $catName ? "cat-active" : ''?>>
-            
-                        <a href="/?cat=<?=$catName?>"><?=$catName?> <span class="small">(
-                                <?=$catNum?>)
-                            </span></a>
-                    </li>
-                    <?php endforeach;?>
-                </ul>
-            </div>
-        </div>
-
-        <div class="newsfeed-content">
-    <!-- Rajouter un test: (1) en cas ou la categorie est non selectionnee -->
-    <?php if (!$selectedCat): ?>
-        <!-- fin test -->
-        <?php foreach ($categories as $cat => $num): ?>
-        <h2>
-            <?=$cat?>
-        </h2>
-        <div class="articles-container">
-            <?php foreach ($articlesParCategorie[$cat] as $a): ?> 
-            <!--Ajouter un lien pour afficher l'article-->
-            <a href="show-article.php?id=<?=$a['id']?>" class="article block">    
-            <!-- <div class="article block">  -->
-                <!--Image-->   
-                <div class="overflow"> 
-                    <div class="img-container" style="background-image:url(<?=$a['image']?>)">
-                    </div>
-                </div>
-                <!-- Titre avec mise en évidence du mot clé *********************************-->
-                <h3>
-                            <?=highlightKeyword($a['titre'], $keyword)?>
-                        </h3>
-                <!--Titre-->
-                <h3>
-                    <?=$a['titre']?>
-                </h3>
-            <!-- </div> -->
-            </a>
-            <?php endforeach;?>
-        </div>
-        <?php endforeach;?>
-        <!--Ajoute un test : (2) en cas ou la categorie est selectionnee-->
-        <?php else: ?>
-        <h2><?=$selectedCat?></h2>
-        <div class="articles-container">
-            <?php foreach ($articlesParCategorie[$selectedCat] as $a): ?>
-                <!--Ajouter un lien pour afficher l'article-->
-                <a href="show-article.php?id=<?=$a['id']?>" class="article block">    
-                <!-- <div class="article block"> -->
-                <!--Image-->
-                <div class="overflow">
-                    <div class="img-container" style="background-image:url(<?=$a['image']?>)">
-                    </div>
                 </div>
 
-                <!-- Titre avec mise en évidence du mot clé***************************** -->
-                <h3>
-                        <?=highlightKeyword($a['titre'], $keyword)?>
-                    </h3>
-                 <!--Titre-->
-                <h3>
-                    <?=$a['titre']?>
-                </h3>
-                <!-- </div> -->
-            </a>
-        <?php endforeach; ?>
+                <div class="menuTitre">
+                    <p>Repas évalués</p>
+                </div>
+
+                <div class="center">
+                <fieldset>
+                    <div class="newsfeed-container">
+                        <div class="categorie-container">
+                            <ul>
+                                <!-- Tous les articles -->
+                                <li class=<?= $selectedCat ? '' : 'cat-active' ?>>
+                                    <a href="/"> Tous les repas <span class="small">(<?= count($articles) ?>)</span></a>
+                                </li>
+                                <!-- Libelles de toutes les categories -->
+                                <?php foreach ($categories as $catName => $catNum) : ?>
+                                    <li class=<?= $selectedCat === $catName ? "cat-active" : '' ?>>
+
+                                        <a href="/?cat=<?= $catName ?>"><?= $catName ?>
+                                            <span class="small">(<?= $catNum ?>)</span>
+                                        </a>
+                                    </li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="newsfeed-content">
+                        <?php if (!$selectedCat) : ?>
+                            <!-- Rajouter un test: (1) En cas où la catégorie n'est pas sélectionnée -->
+                            <?php foreach ($categories as $cat => $num) : ?>
+                                <h1 class="category-title"><?= $cat ?></h1><!--catégorie-->
+                                <div class="articles-container">
+                                    <?php foreach ($articlesParCategorie[$cat] as $a) : ?>
+                                        <!--Ajouter un lien pour afficher l'article-->
+                                        <a href="show-article.php?id=<?= $a['id'] ?>" class="article block">
+                                            <!-- <div class="article block">  -->
+                                            <!--Image-->
+                                            <div class="overflow">
+                                                <div class="img-container" style="background-image:url(<?= $a['image'] ?>)">
+                                                </div>
+                                            </div>
+                                            <!-- Titre avec mise en évidence du mot clé -->
+                                            <h3><?= highlightKeyword($a['titre'], $keyword) ?></h3>
+                                        </a>
+                                    <?php endforeach; ?>
+                                </div>
+                            <?php endforeach; ?>
+                        <?php else : ?>
+                            <!-- Rajouter un test: (2) En cas où une catégorie est sélectionnée -->
+                            <h2><?= $selectedCat ?></h2>
+                            <div class="articles-container">
+                                <?php foreach ($articlesParCategorie[$selectedCat] as $a) : ?>
+                                    <!--Ajouter un lien pour afficher l'article-->
+                                    <a href="show-article.php?id=<?= $a['id'] ?>" class="article block">
+                                        <!--Image-->
+                                        <div class="overflow">
+                                            <div class="img-container" style="background-image:url(<?= $a['image'] ?>)">
+                                            </div>
+                                        </div>
+
+                                        <!-- Titre avec mise en évidence du mot clé -->
+                                        <h3><?= highlightKeyword($a['titre'], $keyword) ?></h3>
+                                    </a>
+                                <?php endforeach; ?>
+                            </div>
+                        <?php endif; ?><!-- Fin du test -->
+
+
+                    </div>
+                </fieldset>
+                </div>
         </div>
-    <?php endif; ?> <!-- Fin du test -->
-
-    
-</div>
-
-    </div>
         <?php require_once 'includes/footer.php' ?>
     </div>
 
@@ -192,8 +185,9 @@ function classifier_articles($acc, $article)
 
 </html>
 
-<?php
-function highlightKeyword($text, $keyword) {
-    return str_ireplace($keyword, '<strong>'.$keyword.'</strong>', $text);
+<?php //recherche du mot clé dans le text
+function highlightKeyword($text, $keyword)
+{
+    return str_ireplace($keyword, '<strong>' . $keyword . '</strong>', $text);
 }
 ?>
